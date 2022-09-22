c17141ff7477f2dd28b8fb7a154a1b26a5efbc90
John Daniels <adeyemijohndaniels@gmial.com>
2022-09-22 08:24:00 +0100

    added appbar and headtext

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'screens/home/home_screen.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -10,11 +12,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: Scaffold(
-            body: Center(child: Image.asset('assets/images/coffee1.jpg'))));
+      title: 'Flutter Demo',
+      theme: ThemeData(),
+      home: const HomeScreen(),
+    );
   }
 }
